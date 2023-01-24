729965e633117313a0ace6f1096fd6123f368987
Bekzat22 <ibila.bekzat@bk.ru>
2023-01-24 16:56:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//
+//ДЗ:
+//        ● Добавить в проект каждому классу героя свою уникальную способность
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        RPG_Game.start();
+    }
+}
